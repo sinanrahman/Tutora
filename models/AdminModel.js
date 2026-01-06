556bac1027ba51e2e6863fd00927fc1e9ae5713d
nadiyakm <kmnadiya003@gmail.com>
2026-01-06 14:35:48 +0530

    models created

---
+++ models/AdminModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const adminSchema = new mongoose.Schema(
+	{
+		fullName: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			lowercase: true,
+		},
+		phone: {
+			type: String,
+		},
+		password: {
+			type: String,
+			required: true,
+			minlength: 8,
+			select: false,
+		},
+		role: {
+			type: String,
+			default: 'ADMIN',
+			immutable: true,
+		},
+		status: {
+			type: String,
+			enum: ['active', 'inactive'],
+			default: 'active',
+		},
+		lastLogin: { type: Date },
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model('Admin', adminSchema);
---
+++ models/StudentModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema(
+	{
+		fullName: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			lowercase: true,
+			trim: true,
+		},
+
+		phone: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		gender: {
+			type: String,
+			enum: ['male', 'female', 'other', 'prefer_not_to_say'],
+			default: 'prefer_not_to_say',
+		},
+
+		country: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+
+		status: {
+			type: String,
+			enum: ['active', 'inactive'],
+			default: 'active',
+		},
+		coordinator: { type: mongoose.Schema.Types.ObjectId, ref: 'Coordinator' },
+	},
+	{
+		timestamps: true,
+	}
+);
+
+module.exports = mongoose.model('Student', studentSchema);
