ff5611461a20fcc7284b6e1e2dff0d5504f874e1
nadiyakm <kmnadiya003@gmail.com>
2026-01-09 10:38:56 +0530

    teacher module

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,16 +1,28 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
 exports.protect = (req, res, next) => {
-	const token = req.cookies.token
+	const token = req.cookies.token;
 
-	if (!token) return res.redirect('/')
+	if (!token) return res.redirect('/');
 
 	try {
-		const decoded = jwt.verify(token, process.env.JWT_SECRET)
-		req.user = decoded
-		next()
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		req.user = decoded;
+		next();
 	} catch (err) {
-		return res.redirect('/')
+		return res.redirect('/');
 	}
-}
+};
 
+exports.isTeacher = (req, res, next) => {
+	if (req.user && req.user.role === 'TEACHER') {
+		return next();
+	}
+	return res.status(403).render('auth/pageNotFound');
+};
+exports.isCoordinator = (req, res, next) => {
+	if (req.user.role !== 'coordinator') {
+		return res.redirect('/');
+	}
+	next();
+};
