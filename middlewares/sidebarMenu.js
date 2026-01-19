92452d34eaf2aa7db34fa5c0f66327dd9b340f75
nadiyakm <kmnadiya003@gmail.com>
2026-01-19 12:08:28 +0530

    sidebar

--- middlewares/sidebarMenu.js
+++ middlewares/sidebarMenu.js
@@ -1,7 +1,7 @@
 const sidebarMenu = require('../config/sidebarMenu');
 const Teacher = require('../models/Teacher');
 const Coordinator = require('../models/Coordinator');
-
+const Admin = require('../models/Admin');
 
 exports.setSidebarMenu = async (req, res, next) => {
 	// 1. Ensure res.locals.user exists by copying req.user or creating an empty object
@@ -24,17 +24,35 @@ exports.setSidebarMenu = async (req, res, next) => {
 				if (teacherDoc && teacherDoc.profilePic) {
 					res.locals.dp = teacherDoc.profilePic.url;
 					// console.log(res.locals.user.dp)
+					res.locals.user.fullName = teacherDoc.fullName;
 				}
 			} catch (err) {
 				console.error('Error fetching teacher profile pic:', err);
 			}
 		}
-        //coordinator
+		// coordinator
 		if (req.user.role === 'COORDINATOR') {
-			const coordinator = await Coordinator.findById(req.user.id).select('fullName');
+			try {
+				const coordinator = await Coordinator.findById(req.user.id).select('fullName');
 
-			if (coordinator) {
-				res.locals.user.fullName = coordinator.fullName;
+				if (coordinator) {
+					res.locals.user.fullName = coordinator.fullName;
+				}
+			} catch (err) {
+				console.error('Error fetching coordinator:', err);
+			}
+		}
+
+		// admin
+		if (req.user.role === 'ADMIN') {
+			try {
+				const admin = await Admin.findById(req.user.id).select('username');
+
+				if (admin) {
+					res.locals.user.fullName = admin.username;
+				}
+			} catch (err) {
+				console.error('Error fetching admin:', err);
 			}
 		}
 
